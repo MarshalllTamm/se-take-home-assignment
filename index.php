d71c95b96618e0a9e8d5ce903fc009551a795c6a
Marshall <marshalltam1230@gmail.com>
2024-05-03 23:32:35 +0800

    done backend

--- index.php
+++ index.php
@@ -17,34 +17,14 @@
     *{
         margin: 0px;
     }
-
-    body{
-        height: 1000vw;
-    }
 </style>
 <body>
-    <!-- "+ Bot" Button & Order buttons -->
-    <!-- Buttons Aligner -->
-    <div class="d-flex p-3 gap-3 justify-content-end position-sticky top-0 bg-secondary mb-3">
-        <!-- Add Icons -->
-        <div class="btn btn-primary remove-bot">
-            Remove Bots
-        </div>
-        <div class="btn btn-primary add-bot">
-            Add Bots
-        </div>
-        <div class="btn btn-primary open-order-modal">
-            New Orders
-        </div>
-        <div class="btn btn-primary open-vip-order-modal">
-            New Vip Orders
-        </div>
-    </div>
 
     <!-- interface area  -->
-    <div class="container-fluid m-0">
-        <div class="row w-100 h-100">
-            <div class="col-6">
+    <div class="container-fluid m-0" style="height: 90vh; max-height: 90vh;">
+        <div class="row h-100">
+            <!-- Bot Area -->
+            <div class="col-6 p-3">
                 <div class="container-fluid">
                     <div class="row justify-content-center h-100 gap-3 bot-area">
 
@@ -52,16 +32,19 @@
                 </div>
             </div>
             <!--  -->
-            <div class="col-6">
-                <!-- Area Select Btn -->
-                <div class="w-100 d-flex justify-content-center gap-3">
-                    <!-- "PENDING" area btn -->
-                    <div class="pending-area-btn btn btn-primary">
-                        Pending
+            <div class="col-6 p-0 my-2 border rounded">
+                <nav>
+                    <div class="nav nav-tabs justify-content-center" id="nav-tab" role="tablist">
+                        <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
+                        <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
                     </div>
-                    <!-- "COMPLETE" area btn -->
-                    <div class="complete-area-btn btn btn-primary">
-                        Complete
+                </nav>
+                <div class="tab-content p-3 pb-0 overflow-hidden" id="nav-tabContent">
+                    <div class="tab-pane fade show active overflow-hidden" style="max-height: 75vh;" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                        
+                    </div>
+                    <div class="tab-pane fade" style="max-height: 75vh;" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                        ...
                     </div>
                 </div>
                 
@@ -133,207 +116,204 @@
         </div>
     </div>
 
+    <!-- "+ Bot" Button & Order buttons -->
+    <!-- Buttons Aligner -->
+    <div class="d-flex p-3 gap-3 justify-content-center bg-secondary" style="height: 10vh;">
+        <!-- Add Icons -->
+        <div class="btn btn-primary remove-bot">
+            Remove Bots
+        </div>
+        <div class="btn btn-primary add-bot">
+            Add Bots
+        </div>
+        <div class="btn btn-primary open-order-modal">
+            New Orders
+        </div>
+        <div class="btn btn-primary open-vip-order-modal">
+            New Vip Orders
+        </div>
+    </div>
+
+    <div id="delete-area">
+    </div>
+
     <!-- jquery -->
 
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script>
-        let total_order_amount = 0; // minus after done
         // NORMAL
-        let regular_order_amount = 0;
+        let regular_order_id = 0; // track the total amount of vip order
+        let regular_order_amount = 0; // track order amount on the queue
+        
         // VIP
-        let vip_order_amount = 0;
+        let vip_order_id = 0; // track the total amount of vip order
+        let vip_order_amount = 0; // track order amount on the queue
 
         //
         let bot_amount = 0;
         let processing = [];
         let order_queue = [];
-        let generated_variable = [false];
 
-        // function start_process(process_time){
-        //     let managing;
-        //     let first_time = true;
-        //     let jay = 0;
+        bot_managing_ = "";
+        interval_ = 0;
 
-        //     process_time = setInterval(function(){
-        //         if(!first_time){
-        //             $("#" + processing[managing]).detach().append(".completed-area");
-        //             order_queue.slice(managing, 1);
-        //             processing.slice(managing, 1);
-    
+        // function start_process(){
+        //     for(let i = 0; i < order_queue.length; i++){
+        //         if(processing.length === 0 && order_queue.length > 0){
+        //             bot_managing_ = order_queue[i];
+        //             processing.push(order_queue[i]);
+        //             console.log(processing);
+        //             console.log(bot_managing_);
+        //             break;
         //         }
+        //         else if(order_queue.length > 0 && processing[i] === undefined){
+        //             bot_managing_ = order_queue[i];
+        //             processing.push(order_queue[i]);
+        //             console.log(processing);
+        //             console.log(bot_managing_);
+        //             break;
+        //         }
+        //     }
 
-        //         let process_length = processing.length;
-        //         first_time = false;
-
-        //         for(let i = 0; i < order_queue.length; i++){
-        //             if(i == process_length){
-        //                 managing = i;
-        //                 console.log(managing);
-        //                 console.log("processing : " + processing);
-        //                 break;
+        //     x = setInterval(function(){
+        //         let temp = processing.findIndex((element) => element == bot_managing_);
+                
+        //         console.log("Currently Processing Queue: " + processing);
+                
+        //         if(temp !== -1){
+        //             if($("#" + bot_managing_).hasClass("regular-order")){
+        //                 $("#" + bot_managing_).removeClass("regular-order");
+        //                 regular_order_amount -= 1;
+        //             }
+        //             else{
+        //                 $("#" + bot_managing_).removeClass("vip-order");
+        //                 vip_order_amount -= 1;
         //             }
 
-        //             if(processing_length === 0){
-        //                 processing.push(order_queue[i]);
-        //                 managing = i;
-        //                 console.log("processing : " + processing);
-        //                 break;
-        //             }    
-
-        //             if(processing[i] == order_queue[i]){
-        //                 continue;
-        //             }   
+        //             $("#" + bot_managing_).addClass("completed-order");
+        //             $(".complete-area").append($("#" + bot_managing_).detach());
+        //             processing.splice(temp, 1);
+        //             order_queue.splice(temp, 1);
+                    
+        //             console.log("Completed: " + bot_managing_);
+        //             bot_managing_ = "";
         //         }
-        //         jay++;
-        //         console.log(jay);
-        //     }, 1000);
+                
+        //         console.log("Processing Queue After Completion: " + processing);
+                
+        //         for(let i = 0; i < order_queue.length; i++){
+        //             if(processing.length === 0 && order_queue.length > 0){
+        //                 bot_managing_ = order_queue[i];
+        //                 processing.push(order_queue[i]);
+        //                 console.log("Added to Process Queue through condition 1: " + processing);
+        //                 break;
+        //             }
+        //             else if(order_queue.length > 0 && processing[i] === undefined){
+        //                 bot_managing_ = order_queue[i];
+        //                 processing.push(order_queue[i]);
+        //                 console.log("Added to Process Queue through condition 1: " + processing);
+        //                 break;
+        //             }
+        //         }
+
+        //         console.log("Currently Processing: " + bot_managing_);
+        //         console.log("Process Queue after interval: " + processing);
+        //     }, 5000);
         // }
-        
-        function start_process(process_time){
-            let first_time = true;
-            let managing;
+         
+        // Bot Quantity Buttons
 
-            setInterval(function(){
-                if(!first_time){
-                    $("#" + processing[managing]).detach().append(".completed-area");
-                    order_queue.slice(managing, 1);
-                    processing.slice(managing, 1);
-    
-                }
-
-                let process_length = processing.length;
-                first_time = false;
-
-                for(let i = 0; i < order_queue.length; i++){
-
-                    if(process_length === 0){
-                        managing = order_queue[i];
-                        processing.push(order_queue[i]);
-                        break;
-                    }                 
-
-                    if(i == process_length){
-                        managing = order_queue[i];
-                        processing.push(order_queue[i]);
-                        break;
-                    }
-
-                    if(processing[i] == order_queue[i]){
-                        continue;
-                    }   
-                }
-
-            }, 1000);
-        }
-
-        // Button Clicks
         $(document).on("click", ".add-bot", function(){
             bot_amount += 1;
-            let generate_new_variable = false;
-
-            if(generated_variable[bot_amount - 1] == false){
-                generate_new_variable = true;
-            }
             
             $.post("processing.php", {
                 add_bot : true,
-                generate_new_variable : generate_new_variable,
                 bot_amount : bot_amount
-
             }, function(data, success){
                 $(".bot-area").append(data);
-
             });
         });
 
         $(document).on("click", ".remove-bot", function(){
-            // alert("hi");
+            $("#delete-area").empty();
+
             $.post("processing.php", {
                 remove_bot : true,
                 bot_amount : bot_amount
-
             }, function(data, success){
-                $("#bot-"+bot_amount).append(data);
-
+                $("#delete-area").append(data);
             });
+            
             $("#bot-"+bot_amount).remove();
-            bot_amount -= 1;
-
+            
+            if(bot_amount > 0){
+                bot_amount -= 1;
+            }
         });
 
+        // Processing Orders
 
         $(document).on("click", ".add-order", function(){  
-
             if($("textarea[name='order-detail']").val().length !== 0){
-                total_order_amount += 1;
                 regular_order_amount += 1;
+                regular_order_id += 1;
 
-                order_queue.push("regular-order-" + total_order_amount);
-                console.log(order_queue);
+                order_queue.push("regular-order-" + regular_order_id);
+                console.log("from add order " + order_queue);
 
                 $.post("processing.php", {
-                    order_amount : regular_order_amount,
+                    order_amount : regular_order_id,
                     order_detail : $("textarea[name='order-detail']").val(),
                     order_quantity : $("input[name='order-quantity']").val(),
                     customer_type : "regular"
-
                 }, function(data, success){
                     $(".pending-area").append(data);
                     $("#order-modal").modal('hide');
-
                 });
-
             }
             else{
                 alert("Please enter something.");
-
             }
         });
 
-        $(document).on("click", ".add-vip-order", function(){          
-
+        $(document).on("click", ".add-vip-order", function(){  
             if($("textarea[name='vip-order-detail']").val().length > 0){
-                total_order_amount += 1;
                 vip_order_amount += 1;
+                vip_order_id += 1;
 
-                // splice(position, delete number, element)
-                order_queue.splice((vip_order_amount - 1), 0, ("vip-order-" + total_order_amount));
-                console.log(order_queue);
+                // splice(position, delete number, element) 
+                order_queue.splice((order_queue.length - regular_order_amount), 0, ("vip-order-" + vip_order_id));
+                console.log("from add vip order " + order_queue);
 
                 $.post("processing.php", {
-                    order_amount : vip_order_amount,
+                    order_amount : vip_order_id,
                     order_detail : $("textarea[name='vip-order-detail']").val(),
                     order_quantity : $("input[name='vip-order-quantity']").val(),
                     customer_type : "vip"
-    
                 }, function(data, success){
                     if($(".regular-order").length === 0){
                         $(".pending-area").append(data);
-
                     }
                     else{
-                        $(data).insertBefore($(".regular-order"));
-
+                        $(data).insertBefore($(".regular-order").eq(0));
                     }
-
                     $("#vip-order-modal").modal('hide');
                 });
-
             }
             else{
                 alert("Please enter something.");
-
             }
+
         });
+
+        // Opening Modals
 
         $(document).on("click", ".open-order-modal", function(){
             $("#order-modal").modal('show');
-            
         });
 
         $(document).on("click", ".open-vip-order-modal", function(){
             $("#vip-order-modal").modal('show');
-
         });
 
     </script>
--- processing.php
+++ processing.php
@@ -33,23 +33,95 @@
         html;
     }
 
+    // add bot script and bot
     if(isset($_POST['add_bot']) && isset($_POST['bot_amount'])){
         echo <<<html
             <div class="col-3 text-center justify-self-center bg-black rounded p-0" style="height: 150px;" id="bot-{$_POST['bot_amount']}">
                 <img src="Images/robot.png" height="150" class="" alt="">
                 <script>
-                    
+                    bot_managing_{$_POST['bot_amount']} = "";
+                    interval_{$_POST['bot_amount']} = 0;
+
+                    function start_process_{$_POST['bot_amount']}(){
+                        for(let i = 0; i < order_queue.length; i++){
+                            if(processing.length === 0 && order_queue.length > 0){
+                                bot_managing_{$_POST['bot_amount']} = order_queue[i];
+                                processing.push(order_queue[i]);
+                                console.log(processing);
+                                console.log(bot_managing_{$_POST['bot_amount']});
+                                break;
+                            }
+                            else if(order_queue.length > 0 && processing[i] === undefined){
+                                bot_managing_{$_POST['bot_amount']} = order_queue[i];
+                                processing.push(order_queue[i]);
+                                console.log(processing);
+                                console.log(bot_managing_{$_POST['bot_amount']});
+                                break;
+                            }
+                        }
+            
+                        interval_{$_POST['bot_amount']} = setInterval(function(){
+                            let temp = processing.findIndex((element) => element == bot_managing_{$_POST['bot_amount']});
+                            
+                            console.log("Currently Processing Queue: " + processing);
+                            
+                            if(temp !== -1){
+                                if($("#" + bot_managing_{$_POST['bot_amount']}).hasClass("regular-order")){
+                                    $("#" + bot_managing_{$_POST['bot_amount']}).removeClass("regular-order");
+                                    regular_order_amount -= 1;
+                                }
+                                else{
+                                    $("#" + bot_managing_{$_POST['bot_amount']}).removeClass("vip-order");
+                                    vip_order_amount -= 1;
+                                }
+            
+                                $("#" + bot_managing_{$_POST['bot_amount']}).addClass("completed-order");
+                                $(".complete-area").append($("#" + bot_managing_{$_POST['bot_amount']}).detach());
+                                processing.splice(temp, 1);
+                                order_queue.splice(temp, 1);
+                                
+                                console.log("Completed: " + bot_managing_{$_POST['bot_amount']});
+                                bot_managing_{$_POST['bot_amount']} = "";
+                            }
+                            
+                            console.log("Processing Queue After Completion: " + processing);
+                            
+                            for(let i = 0; i < order_queue.length; i++){
+                                if(processing.length === 0 && order_queue.length > 0){
+                                    bot_managing_{$_POST['bot_amount']} = order_queue[i];
+                                    processing.push(order_queue[i]);
+                                    console.log("Added to Process Queue through condition 1: " + processing);
+                                    break;
+                                }
+                                else if(order_queue.length > 0 && processing[i] === undefined){
+                                    bot_managing_{$_POST['bot_amount']} = order_queue[i];
+                                    processing.push(order_queue[i]);
+                                    console.log("Added to Process Queue through condition 1: " + processing);
+                                    break;
+                                }
+                            }
+            
+                            console.log("Currently Processing: " + bot_managing_{$_POST['bot_amount']});
+                            console.log("Process Queue after interval: " + processing);
+                        }, 1000);
+                    }
+
+                    start_process_{$_POST['bot_amount']}();
                 </script>
             </div>
         html;
     }
 
+    // append data to delete-area
     if(isset($_POST['remove_bot']) && isset($_POST['bot_amount'])){
-        echo "<script>" . "clearInterval(interval_" . $_POST['bot_amount'] . "); alert({$_POST['bot_amount']});";
         echo <<<html
+            <script>
+                clearInterval(interval_{$_POST['bot_amount']});
+                processing.splice(processing.findIndex((element) => element == bot_managing_{$_POST['bot_amount']}), 1);
 
-            processing.splice(processing.findIndex(managing_{$_POST['bot_amount']}), 1);
-        </script>";
+                delete bot_managing_{$_POST['bot_amount']};
+                delete interval_{$_POST['bot_amount']};
+            </script>
         html;
     }
 
